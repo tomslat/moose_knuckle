906d52746d2d4144400fc4d1c456a3ecbe71551c
tomslat <36338107+tomslat@users.noreply.github.com>
2020-04-04 20:14:33 +0100

    Revert "Revert "Update from Tom""
    This reverts commit 1e038e582c9378e0fc05041b35d57352231177a2.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float turnSpeed;
+    private float angle;
+    private Vector3 input;
+    private Quaternion targetRotation;
+
+    void FixedUpdate()
+    {
+        GetInput();
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+
+    void GetInput()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        input = new Vector3(moveHorizontal, moveVertical, 0.0f);
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, 180, angle);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        transform.position = transform.position + new Vector3(input.x * speed * Time.deltaTime, input.y * speed * Time.deltaTime, 0);
+    }
+}
