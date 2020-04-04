765c9e4e9fd3d208a31222b62332d9a6909dc9c7
tomslat <36338107+tomslat@users.noreply.github.com>
2020-04-04 20:10:08 +0100

    Update from Tom
    Added ship, planet and star.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerObject;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerObject.position.x, playerObject.position.y, transform.position.z);
+    }
+}
